37228f3753d6d9216d5dec9b6d3339710509d818
Patrick Reilly <preilly@princeton.edu>
2017-05-08 15:02:29 -0400

    Added the anonymized pipeline_environment.sh.

---
+++ pipeline_environment.sh
@@ -0,0 +1,19 @@
+#All of these variables must contain the full path to the executables
+#This pipeline depends on:
+#BWA (can obtain via `git clone https://github.com/lh3/bwa --recursive`)
+#Samtools (compatible with versions 1.0+, may have issues with older)
+#Picard (can obtain via `git clone https://github.com/broadinstitute/picard --recursive`)
+#GATK (mainly used with 3.4, all steps but VCFINSNP compatible with newer)
+#seqtk (can obtain via `git clone https://github.com/lh3/seqtk`)
+#STAR (can obtain via `git clone https://github.com/alexdobin/STAR --recursive`)
+
+export BWA="[Path to BWA]/bwa"
+export STAR="[Path to STAR]/STAR"
+export SAMTOOLS="[Path to Samtools]/samtools"
+export PICARD="[Path to Picard]/picard.jar"
+export GATK="[Path to GATK]/GenomeAnalysisTK.jar"
+
+#If you aren't going to use the VCFINSNP step, ignore these three lines:
+export SEQTK="[Path to seqtk]/seqtk"
+export ALISAV9="[Path to PseudoreferencePipeline]/insnp_v9_alisa.py"
+export ALISAV8="[Path to PseudoreferencePipeline]/insnp_v8_alisa.py"
